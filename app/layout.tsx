4042b52465bf2b0398e6d3a4c4e00dd31f692edb
thorfinn <cherfi.m.abdelwahab@gmail.com>
2026-04-30 14:54:47 +0100

    refactor: moved the fonts to a dedicated file for cleaner layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,7 @@
+// app/layout.tsx
 import type { Metadata } from 'next'
 import './globals.css'
-import localFont from 'next/font/local'
-
-export const offBit = localFont({
-  src: [
-    {
-      path: '../public/fonts/OffBit-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/OffBit-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-  variable: '--offbit',
-  display: 'swap',
-})
-
-export const offBit101 = localFont({
-  src: [
-    {
-      path: '../public/fonts/OffBit-101.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/OffBit-101Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-  variable: '--offbit101',
-  display: 'swap',
-})
-
-export const offBitDot = localFont({
-  src: [
-    {
-      path: '../public/fonts/OffBit-Dot.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/OffBit-DotBold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-  variable: '--offbitdot',
-  display: 'swap',
-})
+import { offBit, offBit101, offBitDot } from './fonts'
 
 export const metadata: Metadata = {
   title: 'WABS Lab - Animation Pattern Archive',
@@ -66,7 +16,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={` ${offBit.variable} ${offBit101.variable} ${offBitDot.variable} antialiased`}
+      className={`${offBit.variable} ${offBit101.variable} ${offBitDot.variable} antialiased`}
     >
       <body>
         <main>{children}</main>
